ef3b96d66096bdac053cdc5866d3e1746a4fc29c
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-01-30 12:48:24 +0100

    started work on implementing workspace generation

--- src/projectManagement/projectGenerator.ts
+++ src/projectManagement/projectGenerator.ts
@@ -1,5 +1,19 @@
+import * as path from 'path';
+import { VSCodeUtils } from 'utils/utils';
+import * as vscode from 'vscode';
+
 export class projectGenerator {
-    public static generateWorkspace(){
-        
+    public static async generateWorkspace(): Promise<void>{
+        const files = await vscode.workspace.findFiles('premake5.lua');
+        let workspaceFile: vscode.Uri | undefined = undefined;
+
+        if(files.length > 0){
+            workspaceFile = files[0];
+        } else {
+            workspaceFile = vscode.Uri.file(path.join(VSCodeUtils.getWorkspaceFolder(), 'premake5.lua'));
+            await vscode.workspace.fs.writeFile(workspaceFile, new Uint8Array()); 
+        }
+        const document = await vscode.workspace.openTextDocument(workspaceFile!);
+        await vscode.window.showTextDocument(document);
     }
 }
