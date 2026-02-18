db82b367b6ad132a676f1c925e7e46a4e1606fab
lolrobbe2 <90089980+lolrobbe2@users.noreply.github.com>
2026-02-18 16:18:08 +0100

    fix: docs/generate bugs (#40)
    * Fix bug where allowedAlias lists didn't get [] appended properly in the param annotation
    
    * Fix duplicate allowed values, add additional lowercase to allowedlist
    
    * comment out lower in generate.lua
    
    ---------
    
    Co-authored-by: Myles Cragg <cragg.myles@gmail.com>

--- docs/generate.lua
+++ docs/generate.lua
@@ -1,14 +1,24 @@
 local json = require("json")
 
 local function exportLoweredList()
+    local function contains(arr, item)
+        for k, v in pairs(arr) do
+            if v == item then
+                return true
+            end
+        end
+        return false
+    end
+    
     local output = {}
     for k, v in pairs(premake.field._loweredList) do
         local allowedList = nil
         if v.allowed ~= nil and type(v.allowed) ~= "function"then
             allowedList = {}
             for k, val in pairs(v.allowed) do
-                if type(val) == "string" then
+                if type(val) == "string" and not contains(allowedList, val) then
                     table.insert(allowedList, val)
+                    -- table.insert(allowedList, val:lower())
                 end
             end
         end
